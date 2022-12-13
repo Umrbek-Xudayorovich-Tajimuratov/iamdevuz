ba0f3b6681e82903586ddd7ad233816141cecd94
umrbek.xudayorovich <umrbek.xudayorovich@gmail.com>
2022-12-13 09:39:50 +0500

    navbar branchga o'tish

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import { useState } from "react";
+
 
 
 function App() {
-  const [count, setCount] = useState(0);
+  
 
   return (
-    <div className="App">
-      <h1>Count: {count}</h1>
-      <button onClick={() => setCount(count + 1)}>Increase</button>
-      <button onClick={() => setCount(count - 1)}>Decrease</button>
+    <div className="container">
+      <h1>Siz navbar branchidasiz!</h1>
     </div>
   );
 }
